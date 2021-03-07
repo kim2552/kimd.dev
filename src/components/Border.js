b44ceada3701705922418363ceba0d82dde9e2cd
joohoonkim <davidkim2552@gmail.com>
2021-03-07 17:23:00 -0500

    initial border component

---
+++ src/components/Border.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Border = (props) => {
+    return (
+        <div className="border-container">
+            <h1>{props.title}</h1>
+        </div>
+    )
+}
+
+export default Border
