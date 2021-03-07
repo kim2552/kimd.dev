a70502d8f88cbe73a267a96e975d7a4ad4ec313a
joohoonkim <davidkim2552@gmail.com>
2021-03-07 17:21:03 -0500

    Header, navbar, about and projects component included

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import './scss/app.scss'
 import Header from './components/Header'
+import About from './components/About'
+import Border from './components/Border'
+import Projects from './components/Projects'
+import projects_data from './assets/projects_data.json'
 
 function App() {
+  const projects = projects_data.projects;
+  const border_projects_title = "PROJECTS";
+
   return (
     <div className="container">
       <Header></Header>
+      <div className="content-container">
+        <About></About>
+        <Border title={border_projects_title}></Border>
+        <Projects projects={projects}></Projects>
+      </div>
     </div>
   );
 }
