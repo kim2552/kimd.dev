99b6487693f70d3242c0846bd514a831707e2600
joohoonkim <davidkim2552@gmail.com>
2021-03-08 23:40:40 -0500

    changed components

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
-import './scss/app.scss'
-import Header from './components/Header'
-import About from './components/About'
-import Border from './components/Border'
-import Projects from './components/Projects'
-import Experiences from './components/Experiences'
-import projects_data from './assets/projects_data.json'
-import experience_data from './assets/experience_data.json'
+import './scss/app.scss';
+import Header from './components/Header';
+import Home from './components/Home';
+import About from './components/About';
+import Projects from './components/Projects';
+import Contact from './components/Contact';
+import Footer from './components/Footer';
+
+import projects_data from './assets/projects_data.json';
+import experience_data from './assets/experience_data.json';
+
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 
 function App() {
   const projects = projects_data.projects;
   const experiences = experience_data.experiences;
-  const border_projects_title = "PROJECTS";
-  const border_experience_title = "EXPERIENCE";
 
   return (
     <div className="container">
-      <Header></Header>
-      <div className="content-container">
-        <About></About>
-        <Border title={border_projects_title}></Border>
-        <Projects projects={projects}></Projects>
-        <Border title={border_experience_title}></Border>
-        <Experiences experiences={experiences}></Experiences>
-      </div>
+      <Router>
+        <Header></Header>
+        <div className="content-container">
+            <Switch>
+              <Route path="/about">
+                <About></About>
+              </Route>
+              <Route path="/projects">
+                <Projects projects={projects}></Projects>
+              </Route>
+              <Route path="/contact">
+                <Contact></Contact>
+              </Route>
+              <Route path="/">
+                <Home></Home>
+              </Route>
+            </Switch>
+          <Footer></Footer>
+        </div>
+      </Router>
     </div>
   );
 }
