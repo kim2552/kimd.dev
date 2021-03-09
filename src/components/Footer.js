49005de43274ef5b9c9568955d86dc4f97cae0f5
joohoonkim <davidkim2552@gmail.com>
2021-03-08 23:33:08 -0500

    initial footer

---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Footer() {
+    return (
+        <div className="footer-container">
+            <h3 style={{color: 'black'}}>2021 kimd.dev</h3>
+        </div>
+    )
+}
+
+export default Footer
