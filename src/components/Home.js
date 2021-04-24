8ed22d13a7e52a324d5e7b2fb5eb5bd66d167ff8
joohoonkim <davidkim2552@gmail.com>
2021-04-23 23:22:05 -0400

    changed font. removed image. increased font size.

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,4 +1,3 @@
-import {Link} from 'react-router-dom'
 import {useEffect} from 'react';
 
 const Home = () => {
@@ -13,23 +12,6 @@ const Home = () => {
                     <h1>Hi, my name is David</h1>
                     <h3>Full-stack Software Developer</h3>
                     <h3>Mechatronics B. Eng</h3>
-                </div>
-                <div className="links-wrapper">
-                    <Link to="/about">
-                        <div className="menu-item">
-                            Learn about me!
-                        </div>
-                    </Link>
-                    <Link to="/projects">
-                        <div className="menu-item">
-                            Check out my projects!
-                        </div>
-                    </Link>
-                    <Link to="/contact">
-                        <div className="menu-item">
-                            Get in touch!
-                        </div>
-                    </Link>
                 </div>
             </div>
         </div>
