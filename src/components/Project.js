aa6651d6656d22f8dd62574f5d58463d04afa490
joohoonkim <davidkim2552@gmail.com>
2022-04-16 15:33:56 -0400

    updated projects list, added images for projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -4,20 +4,34 @@ import { FaGithubSquare } from 'react-icons/fa'
 
 const Project = (props) => {
     const p = props.project;
+    var default_link = p.website_link
+    console.log(default_link)
+    if (default_link === undefined) {
+        default_link = p.github_link
+    }
     return (
         <>
-            <h1>{p.title}</h1>
-            <p>{p.text}</p>
-            {(() =>{
-                if(p.website_link){
-                    return <a href={p.website_link} rel="noreferrer"><CgWebsite></CgWebsite></a>
-                }
-            })()}
-            {(() =>{
-                if(p.github_link){
-                    return <a href={p.github_link} rel="noreferrer"><FaGithubSquare></FaGithubSquare></a>
-                }
-            })()}
+            <div style={{display: "flex"}}>
+                <div>
+                    <h1>{p.title}</h1>
+                    <p>{p.text}</p>
+                    {(() =>{
+                    if(p.website_link){
+                        return <a href={p.website_link} rel="noreferrer"><CgWebsite></CgWebsite></a>
+                    }
+                    })()}
+                    {(() =>{
+                        if(p.github_link){
+                            return <a href={p.github_link} rel="noreferrer"><FaGithubSquare></FaGithubSquare></a>
+                        }
+                    })()}
+                </div>
+                <a href={default_link} rel="noreferrer">
+                    <div className="image-wrapper">
+                        <img src={p.image_link} alt="screenshot"></img>
+                    </div>
+                </a>
+            </div>
         </>
     )
 }
